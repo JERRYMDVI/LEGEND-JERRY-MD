74d448762c99ac76e542d27069245524eed67c93
🅂𝙞𝙡𝙚𝙣𝙩 𝙠𝙞𝙡𝙡𝙚𝙧 <166059547+HANDSOMKILLER@users.noreply.github.com>
2025-09-27 07:02:06 +0500

    Update AYTDLL.js

--- plugins/AYTDLL.js
+++ plugins/AYTDLL.js
@@ -3,7 +3,7 @@ const yts = require('yt-search');
 const axios = require('axios');
 
 cmd({
-    pattern: "ytmp4",
+    pattern: "video",
     alias: ["video", "song", "ytv"],
     desc: "Download YouTube videos",
     category: "downloader",
