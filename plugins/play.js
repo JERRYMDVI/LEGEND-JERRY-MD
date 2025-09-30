19bb582daacce2a5f07fe07ea08c4dc126e27513
🅂𝙞𝙡𝙚𝙣𝙩 𝙠𝙞𝙡𝙡𝙚𝙧 <166059547+HANDSOMKILLER@users.noreply.github.com>
2025-09-30 09:51:25 +0500

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -1,7 +1,7 @@
 const { cmd, commands } = require('../lib/command');
 const yts = require('yt-search');
 const { fetchJson } = require('../lib/functions');
-const  { ytmp3 }= require('./lib/scrab')
+const  { ytmp3 }= require('../lib/scrab')
 
 // Function to extract the video ID from youtu.be or YouTube links
 function extractYouTubeId(url) {
